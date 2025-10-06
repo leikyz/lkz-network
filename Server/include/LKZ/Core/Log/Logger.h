64836addc6a35c6451d1862b3b901288cce27337
Leikyz <leikyz17@outlook.com>
2025-10-06 22:48:49 +0200

    fix: add logger files

---
+++ Server/include/LKZ/Core/Log/Logger.h
@@ -0,0 +1,27 @@
+﻿#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+#include "LKZ/Core/Threading/ThreadManager.h"
+#include <format>
+
+enum class LogType
+{
+    Info,
+    Warning,
+    Error,
+    Debug,
+    Received,
+    Sent
+};
+
+class Logger
+{
+public:
+    static void Log(const std::string& message, LogType level = LogType::Info);
+
+private:
+    static std::string GetPrefix(LogType level);
+};
+
+#endif

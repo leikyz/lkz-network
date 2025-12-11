c8749fb413e2522fc3e6b50501acb09d65faff94
Leikyz <leikyz17@outlook.com>
2025-12-11 17:21:49 +0100

    feat: add ImGUI and GLFW feat: start profiler

---
+++ Profiler/Profiler.cpp
@@ -0,0 +1,15 @@
+#include "lib/ImGUI/imgui.h"
+#include "lib/ImGUI/imgui_impl_glfw.h"
+#include "lib/ImGUI/imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h> 
+#include <iostream>
+
+// Configuration
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+int main()
+{
+
+    return 0;
+}
